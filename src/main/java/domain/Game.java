44689e516b821c6d43c53a8e40a8b8395bc88721
root <lmslmsms0616@gmail.com>
2023-05-22 23:53:15 +0900

    Feat: Add Game Initialization Methods 플레이어 인원 수, 플레이어 이름 입력 메소드 추가

---
+++ src/main/java/domain/Game.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("볼링 게임을 시작합니다. Player 수를 입력해주세요.");
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
